a3551a6faf9e6e7e24d44ecb8a86c9133062f39d
mprey <masonprey7@gmail.com>
2024-12-17 21:04:21 -0500

    feat(*): add static values for scaling factors

---
+++ src/main/java/group/gnometrading/objects/Statics.java
@@ -0,0 +1,6 @@
+package group.gnometrading.objects;
+
+public class Statics {
+    public static final long PRICE_SCALING_FACTOR = 1_000_000_000;
+    public static final long SIZE_SCALING_FACTOR = 1_000_000;
+}
